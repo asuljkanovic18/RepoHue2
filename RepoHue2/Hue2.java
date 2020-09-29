be15977f4273cd39fb3ac73a1ec4b85772da898d
unknown <anelsuljkanovic04@gmail.com>
2020-09-29 11:16:49 +0200

    Liste erstellt

---
+++ RepoHue2/Hue2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2.RepoHue2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anels
+ */
+public class Hue2{
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<String> listeS = new ArrayList<String>();
+        listeS.add("Hallo");
+        listeS.add("ok");
+        for (String string : listeS) {
+            System.out.println(listeS);
+        }
+        
+    }
+    
+    
+    
+}
