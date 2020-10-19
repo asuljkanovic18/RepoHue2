8e5e4c9c6079ef143c6ffc98538880d4b38370a1
Anel Suljkanovic <anelsuljkanovic04@gmail.com>
2020-10-19 20:01:28 +0200

    testFile methode fertig

--- RepoHue2/NumberTester.java
+++ RepoHue2/NumberTester.java
@@ -5,6 +5,12 @@
  */
 package hue2.RepoHue2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author anels
@@ -13,14 +19,14 @@ public class NumberTester {
     public interface NumberTest {
         boolean testNumber(int number);
     }
-    private String fileName;
+    private File fileName;
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
     
     
     public NumberTester(String fleName){
-        this.fileName = fileName;
+        fileName = new File(fleName);
     }
     
     public void setOddEvenTester(NumberTest oddTester){
@@ -35,8 +41,59 @@ public class NumberTester {
         this.palindromeTester = palindromeTester;
     }
     
-    public void testFile(){
-        
+    public void testFile() throws FileNotFoundException, IOException{
+        setOddEvenTester(x -> {return x%2==0;});
+        setPrimeTester(p-> {for (int i = 0; i < p+1; i++) {
+                if(p==i*i||p%i==0){
+                    return false;
+                }
+                };return true;}); 
+            setPalindromeTester(pa -> {
+            String left = String.valueOf(pa); 
+            String right = new StringBuilder(left).reverse().toString();
+            return left.equals(right);
+            });
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            int row = Integer.parseInt(br.readLine());
+            int now = 0;
+            
+            String s = br.readLine().trim();
+            int[] numbers = new int[2];
+            
+            while(s != null && now < row){
+                numbers[0] = Integer.parseInt(s.split("")[0]);
+                numbers[1] = Integer.parseInt(s.split("")[1]);
+                
+                int active = numbers[0];
+                int finish = numbers[1];
+                String erg = "";
+                switch(active){
+                    case 1:
+                        if(this.oddTester.testNumber(finish)==true){
+                            erg += " ODD";
+                        }else{
+                            erg += " EVEN";}
+                        break;
+                    case 2:
+                        if(this.primeTester.testNumber(finish)==true){
+                            erg += " PRIME";
+                        }else{
+                            erg += " not PRIME";}
+                        break;
+                    case 3:
+                        if(this.palindromeTester.testNumber(finish)==true){
+                            erg += " PALINDROM";
+                        }else{
+                            erg += " not PALINDROM";}
+                        break;
+                    default: System.out.println("You put in a number out of index");
+                        break;
+                }
+                System.out.println(erg);
+                now++;
+                s = br.readLine().trim();
+            }
+            
     }
     
     
